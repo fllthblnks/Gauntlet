09466f61e3d5d91b80ec6b09238fb7c170e152cf
Guillaume Germain <ggermain@gmail.com>
2020-05-11 23:06:56 +0800

    install modules changes

--- install_modules.sh
+++ install_modules.sh
@@ -1,15 +1,13 @@
 echo 'This will install the required modules for Gauntlet server on your computer.'
 echo 'The script will request your sudo password to install the scripts globally.'
 
-if MACOS then:
-xcode-select --install
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    xcode-select --install
 
-sudo cpan -I HTTP::Server::Simple::CGI
+
 sudo cpan -I POSIX
 sudo cpan -I IO::Socket::INET
 sudo cpan -I IO::Select
 sudo cpan -I Time::HiRes
 sudo cpan -I IO::Handle
-sudo cpan -I Gauntlet::Messaging
-sudo cpan -I Config
 
